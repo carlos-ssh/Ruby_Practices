118f27c033b596336ff8dbcfd988cf725e91ad41
Carlos-SSH <carlos.anezeh@gmail.com>
2020-09-12 20:56:01 -0500

    Create Until loop Solution.

---
+++ control_structures/until.rb
@@ -0,0 +1,15 @@
+=begin
+    
+            Ruby Control Structures - Until
+INSTRUCTIONS:
+
+This challenge has a beautiful one-liner answer.
+"A hacker practices on HackerRank until getting to a rating of O(1) read as (Oh-one)"
+Call the method coder.practice until coder.oh_one? becomes true.
+Use the until control structure.
+until is the logical equivalent of while not.
+
+    
+=end
+
+coder.practice until coder.oh_one?
