eea9186b02288c85d37b42d7d2f3775439053551
Carlos-SSH <carlos.anezeh@gmail.com>
2020-09-14 15:24:14 -0500

    Create Hello HackerRank file.

---
+++ Introduction/Hello_HackerRank.rb
@@ -0,0 +1,13 @@
+
+=begin
+
+*************************** Ruby Tutorial - Hello HackerRank! *************************
+
+INSTRUCTIONS: 
+
+Let's get started with Ruby!
+Go ahead and type the following code in the code-editor:
+
+=end
+
+print "Hello HackerRank!!"
