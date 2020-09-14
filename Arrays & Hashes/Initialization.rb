13b3fcc9168b55380e39d3366e10674526a31bc5
Carlos-SSH <carlos.anezeh@gmail.com>
2020-09-14 16:21:05 -0500

    Create Initialization File.

---
+++ Arrays & Hashes/Initialization.rb
@@ -0,0 +1,36 @@
+=begin
+
+********************* INITIALIZATION ************************
+
+INSTRUCTIONS:
+
+One of the most commonly used data structures in Ruby is a Ruby Array, and below we see various methods of initializing a ruby array.
+Your task is to initialize three different variables as explained below.
+Initialize an empty array with the variable name array
+Hint:
+
+array = Array.new
+or
+
+array = []
+Initialize an array with exactly one nil element in it with the variable name array_1
+Hint:
+
+array_1 = Array.new(1)
+or
+
+array_1 = [nil]
+Initialize an array with exactly two elements with value 10 in it using the variable name array_2.
+Hint
+
+array_2 = Array.new(2, 10)
+or
+
+array_2 = [10, 10]
+
+=end
+
+# Initialize 3 variables here as explained in the problem statement
+array = Array.new
+array_1 = Array.new(1)
+array_2 = Array.new(2, 10)
